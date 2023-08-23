f4651a8c464f1ea37d2225c40bca6ddb7c0ac544
HyoJeong-Park <parkhj3003@gmail.com>
2023-08-23 14:40:53 +0900

    fix: header 수정

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -1,14 +1,16 @@
 import { Link } from 'react-router-dom';
-import { HeaderContainer, LogoVer2Image, IconsWrapper } from './Header.styled';
+import { useSelector } from 'react-redux';
 // import LoginBtn from '../../commons/atoms/buttons/login/LoginBtn';
 // import UserImg from '../../commons/atoms/user/UserImg';
-import logoVer2 from '@/assets/logoVer2.png';
 // import Search from '@/components/search/Search';
 // import { RecuitBtn } from './CHeader.styled';
-import { useSelector } from 'react-redux';
 import { RootState } from '@/store';
+
+import logoVer2 from '@/assets/logoVer2.png';
 import { CgProfile } from 'react-icons/cg';
 import { FiMenu } from 'react-icons/fi';
+
+import { HeaderContainer, LogoVer2Image, IconsWrapper } from './Header.styled';
 
 export default function Header() {
   const loginState = useSelector((state: RootState) => state.loginSlice.isLogin);
@@ -16,24 +18,6 @@ export default function Header() {
 
   return (
     <HeaderContainer>
-      {/* <Link to="/main">
-        <Logo />
-      </Link> */}
-      {/* <Search /> */}
-      {/* <ItemContainer>
-        <CLink href="/boards?division=RECRUITMENT">
-          <RecuitBtn>Community</RecuitBtn>
-        </CLink>
-        {loginState ? (
-          <UserImg />
-        ) : (
-          <LoginContainer>
-            <Link to="/login">
-              <LoginBtn />
-            </Link>
-          </LoginContainer>
-        )}
-      </ItemContainer> */}
       <Link to="/">
         <LogoVer2Image src={logoVer2} alt='logoImage' />
       </Link>
