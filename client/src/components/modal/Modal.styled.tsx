0fbbbc3b3382de1dfcd15ef323aea2411719a931
Emma-Hyejin <cjd191058@naver.com>
2023-08-24 23:33:18 +0900

    Refactor: 알림 모달창 UI 디자인 1차 수정

--- client/src/components/modal/Modal.styled.tsx
+++ client/src/components/modal/Modal.styled.tsx
@@ -5,7 +5,9 @@ export const ModalContainer = tw.div`
   bg-neutral-700/50
   w-full
   h-full
-  absolute
+  fixed
+  flex
+  justify-center
   top-0
   left-0
   z-20
@@ -18,26 +20,33 @@ export const ModalContent = styled.div`
     left-1/2
     bg-white
     rounded-md
-    p-4
+    px-4
+    py-5
     text-center
     z-10
+    w-[420px]
+    flex
+    flex-col
+    items-center
   `}
 
   transform: translate(-50%, -50%);
-  width: 300px;
-  height: 200px;
+  /* width: 300px;
+  height: 200px; */
 
   >h1 {
     ${tw`
       font-bold
       text-2xl
       py-4
+      
     `}
   }
 
   >p {
     ${tw`
       py-2
+      mb-5
     `}
   }
 
@@ -47,7 +56,8 @@ export const ModalContent = styled.div`
     `}
 
     >button {
-      ${tw`mx-3`}
+      ${tw`mx-3`} 
+      margin-bottom: 30px;
     }
   }
 `;
